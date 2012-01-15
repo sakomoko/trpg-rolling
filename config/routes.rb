acc9ccd1d489a87fe5d58dd5b0e5ef6954968a0b
sakomoko <sakomoko@gmail.com>
2012-01-15 18:48:53 +0900

    add users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 TrpgRolling::Application.routes.draw do
 
   devise_for :users
+  resources :users, :only => :show
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
