de01642eecbbe9192888f63658468ed91ed0a751
sakomoko <sakomoko@gmail.com>
2012-01-15 18:13:52 +0900

    add home#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TrpgRolling::Application.routes.draw do
+
   devise_for :users
 
   # The priority is based upon order of creation:
@@ -50,7 +51,7 @@ TrpgRolling::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
