756d5b02eb693f36744c249144a8706b2efdbfba
AIZAWA Sako <sakomoko@gmail.com>
2012-02-23 03:24:45 +0900

    World has_many roomsの設定をroomableに変更した。

--- app/models/world.rb
+++ app/models/world.rb
@@ -5,7 +5,7 @@ class World
 
   belongs_to :owner, class_name: "User", inverse_of: :own_world
   belongs_to :game_system
-  has_many :rooms
+  has_many :rooms, as: :roomable
   has_many :sessions
 
   field :name, :type => String
