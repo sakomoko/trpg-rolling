77e468017adfa6b9e65995adef051c7b2cc717fa
AIZAWA Sako <sakomoko@gmail.com>
2012-02-24 18:57:42 +0900

    fix markdown  helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 module ApplicationHelper
   include BootstrapFormBuilder::FormHelper
+
+  def markdown(text)
+    @renderer ||= Redcarpet::Render::HTML.new(:hard_wrap => true)
+    Redcarpet::Markdown.new(@renderer, :autolink => true, :fenced_code_blocks => true).render(text).html_safe
+  end
 end
