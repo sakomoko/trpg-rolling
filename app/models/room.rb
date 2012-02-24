5d4fa765eb4440bd4883c3a29f454aae33f525b9
AIZAWA Sako <sakomoko@gmail.com>
2012-02-24 21:19:11 +0900

    fixe Room::find_parent_model
    パラメータから親ネストのモデルを捜すメソッド。

--- app/models/room.rb
+++ app/models/room.rb
@@ -13,4 +13,14 @@ class Room
   belongs_to :user
   belongs_to :roomable, polymorphic: true
   validates_presence_of :title, :user
+
+  class << self
+
+    def find_parent_model(params)
+      return Session.find(params[:session_id]) if params[:session_id]
+      return World.find(params[:world_id]) if params[:world_id]
+      nil
+    end
+
+  end
 end
--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,25 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe Room do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#find_parent', do
+    context 'paramsにsession_idが含まれていたとき' do
+      let(:session) { Factory :session }
+      subject { Room.find_parent_model({ :session_id => session.id}) }
+      it { should be_a Session }
+      it { should eq session }
+    end
+
+    context 'paramsにworld_idが含まれていたとき' do
+      let(:world) { Factory :world }
+      subject { Room.find_parent_model({ :world_id => world.id}) }
+      it { should be_a World }
+      it { should eq world }
+    end
+
+    context 'paramsにsession/worldのキーが含まれていないとき' do
+      subject { Room.find_parent_model({ }) }
+      it { should be_nil }
+    end
+  end
 end
