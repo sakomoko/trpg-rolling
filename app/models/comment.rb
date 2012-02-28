0020e29e301a2f70f949027c47e5682e092b876d
AIZAWA Sako <sakomoko@gmail.com>
2012-02-29 05:32:55 +0900

    add validation in Comment Nodel

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,13 @@
 class Comment
   include Mongoid::Document
+  include Mongoid::Timestamps
+
   field :body, :type => String
 
   belongs_to :user
   embedded_in :commentable, polymorphic: true
 
   attr_protected :user_id
+
+  validates_presence_of :body
 end
