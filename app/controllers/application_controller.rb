dd75ee19282a15dcf93916725af3fffd1661e6ea
AIZAWA Sako <sakomoko@gmail.com>
2012-02-29 05:30:59 +0900

    add respond_to js

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ require "application_responder"
 
 class ApplicationController < ActionController::Base
   self.responder = ApplicationResponder
-  respond_to :html, :json
+  respond_to :html, :json, :js
 
   protect_from_forgery
 end
