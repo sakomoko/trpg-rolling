b6936acbc5f7d2d614d0661e9a1e8d6ee3f37d15
AIZAWA Sako <sakomoko@gmail.com>
2012-03-02 02:05:45 +0900

    add validate uniqness for GameSystem

--- app/models/game_system.rb
+++ app/models/game_system.rb
@@ -4,4 +4,5 @@ class GameSystem
   field :description, :type => String
 
   validates_presence_of :name
+  validates_uniqueness_of :name
 end
--- spec/factories/game_systems.rb
+++ spec/factories/game_systems.rb
@@ -1,8 +1,12 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  sequence :game_system_name do |n|
+    "TestGameSystem#{n}}"
+  end
+
   factory :game_system do
-    name "MyString"
+    name {FactoryGirl.generate :game_system_name}
     description "MyString"
   end
 end
