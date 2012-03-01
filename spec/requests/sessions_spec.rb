caf29bed10dd0bc0f53c891297a96c59fdf11d06
AIZAWA Sako <sakomoko@gmail.com>
2012-03-01 15:52:43 +0900

    fix request session spec

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe "Sessions" do
+  let(:world) { Factory :world }
   describe "GET /sessions" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get world_sessions_path(:world_id => 1)
+      get world_sessions_path(:world_id => world.id)
       response.status.should be(200)
     end
   end
