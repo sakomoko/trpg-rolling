2126ca7220ac8f599f35259c6b642a8cab60bea1
AIZAWA Sako <sakomoko@gmail.com>
2012-03-05 07:40:55 +0900

    fix user ability

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'spec_helper'
+require "cancan/matchers"
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "abilities" do
+    subject { ability }
+    let(:ability) { Ability.new(user) }
+    named_let(:user) { Factory :user }
+    context "When editing self own" do
+      it { should be_able_to(:read, user) }
+      it { should be_able_to(:update, user) }
+      it { should_not be_able_to(:create, user) }
+    end
+
+    context "When editing other user" do
+      named_let(:other_user) { Factory :user }
+      it { should be_able_to(:read, other_user) }
+      it { should_not be_able_to(:update, other_user) }
+      it { should_not be_able_to(:create, other_user) }
+      it { should_not be_able_to(:destroy, other_user) }
+    end
+  end
 end
