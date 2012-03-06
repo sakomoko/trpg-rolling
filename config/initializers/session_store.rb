711db86cefc7a087e7ae30918f19cc919f7b16ef
AIZAWA Sako <sakomoko@gmail.com>
2012-03-07 03:00:51 +0900

    update to rails3.2.2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-TrpgRolling::Application.config.session_store :cookie_store, key: '_trpg-rolling_session'
+TrpgRolling::Application.config.session_store :cookie_store, key: '_trpg_rolling_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
