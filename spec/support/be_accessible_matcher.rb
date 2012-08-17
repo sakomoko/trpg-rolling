31628795ca4ae14c2067311567a65d9136096164
AIZAWA Sako <sakomoko@gmail.com>
2012-08-18 07:37:14 +0900

    add be_admin_accesible matcher

--- spec/support/be_accessible_matcher.rb
+++ spec/support/be_accessible_matcher.rb
@@ -6,3 +6,12 @@ RSpec::Matchers.define :be_accessible do |attribute|
   failure_message_for_should { ":#{attribute} should be accessible" }
   failure_message_for_should_not { ":#{attribute} should not be accessible" }
 end
+
+RSpec::Matchers.define :be_admin_accessible do |attribute|
+  match do |response|
+    response.class.accessible_attributes(:admin).include?(attribute)
+  end
+  description { "be accessible :#{attribute}" }
+  failure_message_for_should { ":#{attribute} should be accessible" }
+  failure_message_for_should_not { ":#{attribute} should not be accessible" }
+end
