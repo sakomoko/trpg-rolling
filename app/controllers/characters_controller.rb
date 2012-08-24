cb7f0cb2c8bd17a141d609463e27bf6d597f6bcb
AIZAWA Sako <sakomoko@gmail.com>
2012-08-24 10:35:20 +0900

    add characters controller and views.

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,8 @@
 class CharactersController < ApplicationController
+
+  def index
+    @world = World.find params[:world_id]
+    @system_key = @world.game_system.system_key.underscore
+    @characters = @world.characters
+  end
 end
--- spec/controllers/characters_controller_spec.rb
+++ spec/controllers/characters_controller_spec.rb
@@ -2,4 +2,19 @@ require 'spec_helper'
 
 describe CharactersController do
 
+  let(:world) { FactoryGirl.create :world, :game_system => game_system }
+  let(:game_system) { FactoryGirl.create :game_system, :system_key => "SwordWorldRpg2"}
+  let(:character) { FactoryGirl.create :character, :world => world }
+  let(:other_character) { FactoryGirl.create :character, :world => world }
+  let(:other_world_character) { FactoryGirl.create :character}
+
+  describe "GET index" do
+    subject { assigns :characters }
+    before do
+      get :index, world_id: world.to_param
+    end
+    its(:to_a) { should eq [character, other_character] }
+    it { assigns(:world).should eq character.world }
+    it { assigns(:system_key).should eq game_system.system_key.underscore }
+  end
 end
