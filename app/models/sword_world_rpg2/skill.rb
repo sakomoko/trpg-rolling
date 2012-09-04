16fc54850bb8c020c5566e6781268b4f820d69f6
AIZAWA Sako <sakomoko@gmail.com>
2012-09-04 17:43:02 +0900

    add SworldWorldRpg2::skill.create_seed_data

--- app/models/sword_world_rpg2/skill.rb
+++ app/models/sword_world_rpg2/skill.rb
@@ -6,4 +6,14 @@ class SwordWorldRpg2::Skill
 
   validates_presence_of :name
   attr_accessible :name, :has_mp, as: [:default, :admin]
+
+  class << self
+    def create_seed_data
+      skills = YAML.load_file "#{Rails.root}/db/seeds/sword_world_rpg2/skills.yml"
+      self.delete_all
+      skills.each do |skill|
+        self.create! skill, as: :admin
+      end
+    end
+  end
 end
--- spec/models/sword_world_rpg2/skill_spec.rb
+++ spec/models/sword_world_rpg2/skill_spec.rb
@@ -20,4 +20,13 @@ describe SwordWorldRpg2::Skill do
     it { should allow_mass_assignment_of(:name).as(:admin) }
     it { should allow_mass_assignment_of(:has_mp).as(:admin) }
   end
+
+  describe "create_seed_data" do
+    subject { SwordWorldRpg2::Skill.count}
+    let(:skills) {YAML.load_file "#{Rails.root}/db/seeds/sword_world_rpg2/skills.yml"}
+    before do
+      SwordWorldRpg2::Skill.create_seed_data
+    end
+    it { should eq skills.count }
+  end
 end
